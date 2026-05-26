131152d03a15035c8966e244c56233dd9f48d5e7
Stvel <steve6731943@qq.com>
2026-05-26 20:55:58 +0200

    iframeView

---
+++ js/iframeView.js
@@ -0,0 +1,51 @@
+let currentTop = 0; let currentLeft = 0;
+let iframeView = document.getElementById("iframeView");
+let moveIframe = false;
+
+function moveIframeToggle(element){
+   moveIframe = !moveIframe;
+   if (moveIframe){
+      iframe.style.pointerEvents= "none";
+      element.style.border = "1px solid blue"
+   }else{
+      iframe.style.pointerEvents= "auto";
+      element.style.border = ""
+   }
+}
+
+iframeView.addEventListener('pointerdown', function (e) {
+   //Si haces clic con el boton izquierdo del raton
+   if (e.button === 0 ) {
+      console.log("do pointer down")
+         dragging = true;
+         let matrix = iframe.style.translate.split(' ');;
+         currentTop = parseFloat(matrix[1]) || 0;
+         currentLeft = parseFloat(matrix[0]) || 0;
+         iframeView.mouseLastY = e.clientY;
+         iframeView.mouseLastX = e.clientX;
+         iframeView.mouseStartY = currentTop;
+         iframeView.mouseStartX = currentLeft;
+         e.stopPropagation();
+   }
+});
+
+document.addEventListener('pointermove', function (e) {
+   if (dragging) {
+      // calcula cuanto moueve el raton y obtener nuevo posicion del scroll
+      let mouseMoveLengthY = e.clientY - iframeView.mouseLastY;
+      let mouseMoveLengthX = e.clientX - iframeView.mouseLastX;
+      iframeView.mouseLastY = e.clientY;
+      iframeView.mouseLastX = e.clientX;
+      currentTop = currentTop + mouseMoveLengthY;
+      currentLeft = currentLeft + mouseMoveLengthX;
+
+      iframe.style.translate = `${currentLeft}px ${currentTop}px`;
+   }
+});
+
+document.addEventListener('pointerup', function (e) {
+   if (dragging) {
+      e.stopPropagation();
+      dragging = false;
+   }
+});
